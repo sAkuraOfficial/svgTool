c25e122901a6bd057f0951f116f808981e5f67ea
sAkuraOfficial <mchaohao159@outlook.com>
2026-02-20 18:16:58 +0800

    chore: make vite base path apply only on github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/svgTool/',
+  base: process.env.GITHUB_ACTIONS ? '/svgTool/' : '/',
   plugins: [
     react(),
     tailwindcss(),
